67e853d092234bce5b03772824356277893c56ae
Lucas Caetano <t-lucas.caetano@cni.com.br>
2025-09-02 22:26:26 -0300

    feat: testing using documentation

---
+++ main.js
@@ -0,0 +1,28 @@
+const { app, BrowserWindow, nativeTheme } = require('electron')
+
+const createWindow = () => {
+    nativeTheme.themeSource = 'light'
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    icon: './src/public/img/image.png',
+    // resizable: false,
+    // autoHideMenuBar: true,
+    // titleBarStyle: 'hidden',
+
+  })
+
+  win.loadFile('./src/views/index.html')
+}
+
+app.whenReady().then(() => {
+  createWindow()
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow()
+  })
+})
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') app.quit()
+  })
